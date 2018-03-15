0d5dd26e42ca5ed7d46c4dfffc845afe610a14d6
jouyouyun <jouyouwen717@gmail.com>
2018-03-15 09:38:53 +0800

    list: improve helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -39,13 +39,33 @@
  helm-ff-search-library-in-sexp t
  ;; scroll 8 lines other window using M-<next>/M-<prior>
  helm-scroll-amount 8
- helm-ff-file-name-history-use-recentf t)
+ helm-ff-file-name-history-use-recentf t
+ helm-echo-input-in-header-line t)
 
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
 
 (when (and (executable-find "ag")
            (maybe-require-package 'ag))
-    (require-package 'helm-ag)
-    (setq-default ag-highlight-search t)
-    (global-set-key (kbd "M-?") 'helm-ag))
+  (require-package 'helm-ag)
+  (setq-default ag-highlight-search t)
+  (global-set-key (kbd "M-?") 'helm-ag))
+
+(helm-mode 1)
 
 (provide 'init-helm)
